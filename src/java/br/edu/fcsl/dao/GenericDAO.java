dca77eb8ec59aeffe0df7b9e2fa5e17a7062f7b7
darlisson <darlisson@casa>
2016-06-03 19:21:54 -0300

    Criação dos DAO

---
+++ src/java/br/edu/fcsl/dao/GenericDAO.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fcsl.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author darlisson
+ */
+public class GenericDAO<T>{
+     private EntityManager em;
+    private Class classe;
+
+    public GenericDAO(Class classe) {
+        em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        this.classe = classe;
+    }
+
+    public void salvar(T objeto) {
+        em.getTransaction().begin();
+        em.merge(objeto);
+        em.getTransaction().commit();
+
+    }
+
+    public void excluir(T objeto) {
+        em.getTransaction().begin();
+        em.remove(objeto);
+        em.getTransaction().commit();
+
+    }
+
+    public T recuperar(Long id) {
+
+        return (T) em.find(classe, id);
+    }
+
+    public List<T> listar() {
+        return em.createQuery("fron" + classe.getSimpleName() +"e").getResultList();
+    }
+
+    
+}
