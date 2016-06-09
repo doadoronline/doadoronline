dc8146c90b3aa37016a8c1fc28bc268b8869af2a
vseliasxd <viniciusem@gmail.com>
2016-06-08 21:23:59 -0300

    Adição do template e configuração da interpretação das URLS do JSF. Para abrir uma página, basta acionar a url padrão /nomeDaPagina.jsf

--- src/java/br/edu/fcsl/entidade/Solicitacao.java
+++ src/java/br/edu/fcsl/entidade/Solicitacao.java
@@ -13,8 +13,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.ws.rs.Encoded;
-
 /**
  *
  * @author Elias
