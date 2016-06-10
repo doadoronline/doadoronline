9766db8d9adc74a182ee3050117cbc73192232ae
Thiago <thiago.ar17@gmail.com>
2016-06-10 00:09:51 -0300

    Crimando MB do produto

---
+++ src/java/br/edu/fcsl/mb/ProdutoMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fcsl.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author thiago
+ */
+@ManagedBean
+@ViewScoped
+public class ProdutoMB {
+
+    /**
+     * Creates a new instance of ProdutoMB
+     */
+    public ProdutoMB() {
+    }
+    
+}
