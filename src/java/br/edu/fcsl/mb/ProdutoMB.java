f46b6a77760f5af4fe885fb4c1bdcc09dea5d6c9
Thiago <thiago.ar17@gmail.com>
2016-06-10 00:04:17 -0300

    Criando menu lateral para produto

---
+++ src/java/br/edu/fcsl/mb/ProdutoMB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fcsl.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author thiago
+ */
+@ManagedBean
+@RequestScoped
+public class ProdutoMB {
+
+    
+}
